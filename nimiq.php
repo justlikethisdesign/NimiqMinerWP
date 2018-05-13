fd2c19e7bb03a88e186a072663bd31aecc67889a
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-05-13 02:18:01 +0100

    Inital Commit - Just the basics for now

---
+++ nimiq.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Nimiq Miner WP
+Plugin URI: https://github.com/justlikethisdesign/NimiqMinerWP
+Description: A cryptocurrency miner for your Wordpress website. Powered by Nimiq.
+Version: 0.0.1
+Author: Robert Marshall
+Author URI: https://github.com/justlikethisdesign
+Tags: crypto, cryptocurrency, miner, nimiq
+License: GPL3
+*/
+
+include ( 'inc/config.php' );
+
+include ( 'inc/enqueue_scripts.php' );
+
+include ( 'inc/settings/register.php' );

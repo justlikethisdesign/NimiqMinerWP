2afb4b53b4eb9cc51aa249e7eccf21ba29b7cfe6
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-05-13 02:16:16 +0100

    no message

--- nimiq/js/config.js
+++
@@ -1,8 +0,0 @@
-//config
-var myNimiqAddress ="NQ30 TUC5 LCQA F0QU RCEP YXYP AN5M NDPM E4DR";
-var percentOfThread = 1;
-var pool = true;
-var areYouNice = true;
-var logsOn = false;
-var poolAddress ="eu.sushipool.com";
-var poolPort ="443";
